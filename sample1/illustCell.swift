f609e5a13b75a604e86fd8cf6a382d20d8775f26
小山雅斗 <yattyo@koyamatadashitonoMacBook-Pro.local>
2017-04-10 20:45:16 +0900

    イラストセルのファイルを作りました。

---
+++ sample1/illustCell.swift
@@ -0,0 +1,12 @@
+//
+//  illustCell.swift
+//  sample1
+//
+//  Created by 小山雅斗 on 2017/04/10.
+//  Copyright © 2017年 pixiv.Inc. All rights reserved.
+//
+
+import UIKit
+
+class IllustCell: UITableViewCell {
+}
