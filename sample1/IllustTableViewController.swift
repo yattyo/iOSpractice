a1adb6774924a93278d6279f4f3f639b6eb13cab
小山雅斗 <yattyo@koyamatadashitonoMacBook-Pro.local>
2017-04-10 20:35:07 +0900

    commit

---
+++ sample1/IllustTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IllustTableViewController.swift
+//  sample1
+//
+//  Created by 小山雅斗 on 2017/04/10.
+//  Copyright © 2017年 pixiv.Inc. All rights reserved.
+//
+
+import UIKit
+
+// UITableViewControllerを継承
+class IllustTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
